c0cac00e9e07857ba0a7b0cb722b33ab20660ef0
roland372 <damian372@poczta.onet.pl>
2020-07-27 14:23:48 +0200

    Initial commit

---
+++ 1-MongoDB-Express-Setup/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+// get values from default.json file
+const db = config.get('mongoURI');
+
+// connect to db
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			// add configuration
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('MongoDB Connected');
+	} catch (err) {
+		console.error(err.message);
+		// quit application with failure
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
